7ae0fa457890223b7c4d710ce268f9708b1c0a48
Alssdev <alssdev3@gmail.com>
2024-10-03 12:17:53 -0600

    fix: adding astro.config.mjs/base attribute

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alssdev.github.io/cc8-physical-layer-rfc",
+  site: "https://alssdev.github.io",
+  base: "/cc8-physical-layer-rfc",
   markdown: {
     smartypants: true,
     syntaxHighlight: "shiki",
