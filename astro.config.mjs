4ec37166ba97829436af3fe04180092118258836
Alssdev <alssdev3@gmail.com>
2024-10-03 12:13:04 -0600

    fix: custom github pages url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alssdev.github.io/cc8-physical-layer-rfc/",
+  site: "https://alssdev.github.io/cc8-physical-layer-rfc",
   markdown: {
     smartypants: true,
     syntaxHighlight: "shiki",
